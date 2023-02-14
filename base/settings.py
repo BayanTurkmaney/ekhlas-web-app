d13510ff803e0a1d399b197e337163f1d7197117
BayanTurkmaney <58312288+BayanTurkmaney@users.noreply.github.com>
2023-02-14 23:37:47 +0300

    Update settings.py

--- base/settings.py
+++ base/settings.py
@@ -102,10 +102,10 @@ WSGI_APPLICATION = 'base.wsgi.application'
 # }
 # }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -140,9 +140,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+# STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
+
 # MEDIA_URL = '/media/'
 MEIDA_ROOT ='/media/'
 MEIDA_ROOT = '/img/'
