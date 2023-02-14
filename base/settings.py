aa83fa7d9b29817a403b9f7b20cbb54ee2b230a6
BayanTurkmaney <58312288+BayanTurkmaney@users.noreply.github.com>
2023-02-14 20:10:50 +0300

    Update settings.py

--- base/settings.py
+++ base/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#16($85b4fl243+b*wgcbfckcdjm!erq45&n1vr^yv9@=)o2v&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #.vercel.app
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
