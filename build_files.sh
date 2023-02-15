b94bda46aef85ebeb13a9c1552ebeaa5f92624f2
BayanTurkmaney <58312288+BayanTurkmaney@users.noreply.github.com>
2023-02-15 15:32:51 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,4 @@
-echo " BUILD START"
+pip install -r requirements.txt
+python3.10 manage.py collectstatic
 
-python3.10 -m pip install -r requirements.txt
-
-python3.10 manage.py collectstatic --noinput --clear
-
-echo " BUILD END"
 
